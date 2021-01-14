7d303b966a954a5f9455e8b24b561270f5643019
MaiNguyen999 <maikhanhca2019@gmail.com>
2021-01-14 14:20:42 -0600

    initial commit

---
+++ precache-manifest.0b61db90a67ffe25d5c54ff64e09762f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa54127741511976958",
+    "url": "/static/css/main.548c32dd.chunk.css"
+  },
+  {
+    "revision": "eaa54127741511976958",
+    "url": "/static/js/main.eaa54127.chunk.js"
+  },
+  {
+    "revision": "9b31fbe82231f4d94d4a",
+    "url": "/static/js/runtime~main.9b31fbe8.js"
+  },
+  {
+    "revision": "d6b7485029e081bef29e",
+    "url": "/static/css/2.6f7c09e4.chunk.css"
+  },
+  {
+    "revision": "d6b7485029e081bef29e",
+    "url": "/static/js/2.d6b74850.chunk.js"
+  },
+  {
+    "revision": "ca8c90e0183bcd33acc3ac3242d3f97e",
+    "url": "/static/media/logo_light.ca8c90e0.svg"
+  },
+  {
+    "revision": "fec6134b9773126fdc216a985067485f",
+    "url": "/static/media/circle.fec6134b.svg"
+  },
+  {
+    "revision": "6d8c72abae59fb2b863e7900f3942197",
+    "url": "/static/media/logo_dark.6d8c72ab.svg"
+  },
+  {
+    "revision": "c952bed47cbb883cc98859f87fad8597",
+    "url": "/index.html"
+  }
+]);
